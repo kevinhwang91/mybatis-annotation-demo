613f4eb0289fe0250dac8bf8bfa97f3af11f60df
kevinhwang91 <kevin.hwang@live.com>
2018-04-28 01:21:50 +0800

    Initial commit

---
+++ src/main/java/com/jackson/puppy/mybatis/Application.java
@@ -0,0 +1,25 @@
+package com.jackson.puppy.mybatis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		final ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			logger.info(beanName);
+		}
+	}
+}
